569f54bfa84f6718787cfa4a6799a92925fdaaa9
mikhael <sur.mikhael@gmail.com>
2024-07-26 17:09:04 +0300

    added generics to display information in the api replaced BaseModel with Schema from ninja BaseModel doesn't want to be friends with generics

--- core/api/schemas.py
+++ core/api/schemas.py
@@ -1,5 +1,28 @@
-from pydantic import BaseModel
+from typing import Any, Generic, TypeVar
+
+from ninja import Schema, Field
 
 
-class PingResponseSchema(BaseModel):
+TData = TypeVar("TData")
+TListItem = TypeVar("TListItem")
+
+
+class PingResponseSchema(Schema):
     result: str
+
+
+class PaginationOut(Schema):
+    offset: int
+    limit: int
+    total: int
+
+
+class ListPaginatedResponse(Schema, Generic[TListItem]):
+    items: list[TListItem]
+    pagination: PaginationOut
+
+
+class ApiResponse(Schema, Generic[TData]):
+    data: TData | dict = Field(default_factory=dict)
+    meta: dict[str, Any] = Field(default_factory=dict)
+    errors: list[Any] = Field(default_factory=list)
--- core/api/v1/products/handlers.py
+++ core/api/v1/products/handlers.py
@@ -1,15 +1,24 @@
 from django.http import HttpRequest
 from ninja import Router
 
-from core.api.v1.products.schemas import ProductListSchema, ProductSchema
+from core.api.schemas import ApiResponse, ListPaginatedResponse, PaginationOut
+from core.api.v1.products.schemas import ProductSchema
 from core.apps.products.services.products import BaseProductsService, ORMProductsService
 
 router = Router(tags=["Products"])
 
 
-@router.get("", response=ProductListSchema)
-def get_product_list_handler(request: HttpRequest) -> ProductListSchema:
+@router.get("", response=ApiResponse[ListPaginatedResponse[ProductSchema]])
+def get_product_list_handler(request: HttpRequest) -> ApiResponse[ListPaginatedResponse[ProductSchema]]:
     service: BaseProductsService = ORMProductsService()
     product_list = service.get_product_list()
+    product_count = service.get_product_count()
+    items = [ProductSchema.from_entity(obj) for obj in product_list]
+    pagination = PaginationOut(offset=0, limit=10, total=product_count)
 
-    return [ProductSchema.from_entity(obj) for obj in product_list]
+    return ApiResponse(
+        data=ListPaginatedResponse(
+            items=items,
+            pagination=pagination
+        )
+    )
